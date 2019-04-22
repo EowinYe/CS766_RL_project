fa4d18b11b7d5ab193c2e05f4660ac0e9fd83450
EowinYe <eowinye@gmail.com>
2019-04-22 04:49:06 -0500

    updata memory ppo

--- ppo_simple.py
+++ ppo_simple.py
@@ -5,7 +5,6 @@ import numpy as np
 import tensorflow as tf
 from skimage.color import rgb2gray
 from skimage.transform import resize
-from collections import deque
 
 ENV_NAME = 'Breakout-v0'  # Environment name
 BATCHSIZE = 32
@@ -16,13 +15,10 @@ STATE_LENGTH = 4  # Number of most recent frames to produce the input to the net
 A_LR = 2.5e-4
 C_LR = 5e-4
 EPSILON = 0.2
-UPDATE_STEPS = 10
+UPDATE_STEPS = 4
 TIME_HORIZON = 128
 GAMMA = 0.99                 # reward discount
 NO_OP_STEPS = 30  # Maximum number of "do nothing" actions to be performed by the agent at the start of an episode
-START_LEARNING = 20000
-MEMORY_CAPACITY = 400000
-TRAIN_INTERVAL = 4  # The agent selects 4 actions between successive updates
 TRAIN = True
 LOAD_NETWORK = False
 SAVE_INTERVAL = 1000  # The frequency with which the network is saved
@@ -41,9 +37,7 @@ class PolicyGradient:
         self.n_actions = n_actions
         self.gamma = reward_decay
 
-        self.memory = deque()
-
-        self.learn_step_counter = 0s
+        self.memory = [[], [], []]
 
         self.total_reward = 0
         self.total_loss = 0
@@ -137,72 +131,62 @@ class PolicyGradient:
     def choose_action(self, state):
         # act_prob = self.pi.eval(feed_dict={self.s: [np.float32(state / 255.0)]})
         # action = np.random.choice(range(act_prob.shape[1]), p=act_prob.ravel())
-        action = self.action.eval(feed_dict={self.s: [np.float32(state)]})[0]
+        action = self.action.eval(feed_dict={self.s: [np.float32(state / 255.0)]})[0]
         return action
 
     def get_initial_state(self, observation, last_observation):
         processed_observation = np.maximum(observation, last_observation)
-        processed_observation = np.array((resize(rgb2gray(processed_observation), (WIDTH, HEIGHT)) * 255)/255, dtype=np.float32)
+        processed_observation = np.uint8(resize(rgb2gray(processed_observation), (WIDTH, HEIGHT)) * 255)
         state = [processed_observation for _ in range(STATE_LENGTH)]
         return np.stack(state, axis=0)
 
-    def train_network(self):
+    def train_network(self, state_batch, action_batch, reward_batch, last_reward):
+        discount_r = np.zeros_like(reward_batch)
+        for i in range(len(reward_batch)-1, -1, -1):
+            last_reward = reward_batch[i] + self.gamma*last_reward
+            discount_r[i] = last_reward
+        discount_r = discount_r[:, np.newaxis]
+
         self.sess.run(self.update_oldpi_op)
+        adv = self.advantage.eval(feed_dict={
+            self.s: state_batch,
+            self.r: discount_r})
 
         for i in range(UPDATE_STEPS):
-            state_batch = []
-            action_batch = []
-            reward_batch = []
-            next_state_batch = []
-            terminal_batch = []
-            y_batch = []
+            for j in range(TIME_HORIZON//BATCHSIZE):
+                if j*BATCHSIZE >= discount_r.shape[0]:
+                    break
+                sbj = state_batch[j*BATCHSIZE:(j+1)*BATCHSIZE]
+                abj = action_batch[j*BATCHSIZE:(j+1)*BATCHSIZE]
+                advbj = adv[j*BATCHSIZE:(j+1)*BATCHSIZE]
+                drbj = discount_r[j*BATCHSIZE:(j+1)*BATCHSIZE]
+                aloss, _ =  self.sess.run([self.aloss, self.atrain_op], feed_dict={
+                    self.s: sbj,
+                    self.a: abj,
+                    self.adv: advbj})
+                self.total_loss += aloss
 
-            minibatch = random.sample(self.memory, BATCH_SIZE)
-            for data in minibatch:
-                state_batch.append(data[0])
-                action_batch.append(data[1])
-                reward_batch.append(data[2])
-                next_state_batch.append(data[3])
-                terminal_batch.append(data[4])
-
-            terminal_batch = np.array(terminal_batch) + 0
-
-            target_values_batch = self.v.eval(feed_dict={self.s: np.float32(next_state_batch)})
-            y_batch = reward_batch + (1 - terminal_batch) * GAMMA * target_values_batch
-
-            state_batch = np.float32(state_batch)
-
-            adv = self.advantage.eval(feed_dict={
-                self.s: state_batch,
-                self.r: y_batch})
-
-            aloss, _ =  self.sess.run([self.aloss, self.atrain_op], feed_dict={
-                self.s: state_batch,
-                self.a: action_batch,
-                self.adv: adv})
-            self.total_loss += aloss
-
-            closs, _ =  self.sess.run([self.closs, self.ctrain_op], feed_dict={
-                self.s: state_batch,
-                self.r: y_batch})
-            self.total_loss += closs
+                closs, _ =  self.sess.run([self.closs, self.ctrain_op], feed_dict={
+                    self.s: sbj,
+                    self.r: drbj})
+                self.total_loss += closs
 
     def learn(self, state, action, reward, done, observation):
         next_state = np.append(state[1:, :, :], observation, axis=0)
         reward = np.clip(reward, -1, 1)
 
-        self.memory.append((state, action, reward, next_state, done))
-        if len(self.memory) > MEMORY_CAPACITY:
-            self.memory.popleft()
+        self.memory[0].append(state)
+        self.memory[1].append(action)
+        self.memory[2].append(reward)
 
         self.total_reward += reward
         self.time += 1
 
-        self.learn_step_counter += 1
-
-        if self.learn_step_counter >= START_LEARNING:
-            if self.learn_step_counter % TRAIN_INTERVAL == 0:
-                self.train_network()
+        if self.time % TIME_HORIZON == 0 or done:
+            v_ = self.v.eval(feed_dict={self.s: [np.float32(next_state / 255.0)]})
+            self.train_network(np.float32(np.array(self.memory[0]) / 255.0),
+                np.array(self.memory[1]), np.array(self.memory[2]), v_[0])
+            self.memory = [[], [], []]
 
         if done:
             if self.episode % SAVE_INTERVAL == 0:
@@ -251,7 +235,7 @@ class PolicyGradient:
 
 def preprocess(observation, last_observation):
     processed_observation = np.maximum(observation, last_observation)
-    processed_observation = np.array((resize(rgb2gray(processed_observation), (WIDTH, HEIGHT)) * 255)/255, dtype=np.float32)
+    processed_observation = np.uint8(resize(rgb2gray(processed_observation), (WIDTH, HEIGHT)) * 255)
     return np.reshape(processed_observation, (1, WIDTH, HEIGHT))
 
 
