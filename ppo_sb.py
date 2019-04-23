2123abfa5c45139cc3f7d88f1610b6d604b20d90
EowinYe <eowinye@gmail.com>
2019-04-23 00:11:24 -0700

    updata ppo

--- ppo_sb.py
+++ ppo_sb.py
@@ -5,6 +5,7 @@ from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO1
 
 ENV_NAME = 'Breakout-v0'
+SAVE_NETWORK_PATH = 'saved_networks/PPO_stable_baselines/PPO1_' + ENV_NAME
 TRAIN = True
 
 env = gym.make(ENV_NAME)
@@ -13,11 +14,11 @@ env = DummyVecEnv([lambda: env])
 if TRAIN:
     model = PPO1(CnnPolicy, env, verbose=1)
     model.learn(total_timesteps=1000000)
-    model.save("PPO1_"+ENV_NAME)
+    model.save(SAVE_NETWORK_PATH)
 
     del model
 else:
-    model = PPO1.load("PPO1_"+ENV_NAME)
+    model = PPO1.load(SAVE_NETWORK_PATH)
     obs = env.reset()
     while True:
         action, _states = model.predict(obs)
