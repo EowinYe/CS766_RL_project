c939b218470f876e8402adb2d5ec5c3814d107f8
EowinYe <eowinye@gmail.com>
2019-04-24 11:48:12 -0700

    updata ppo on breakout

--- ppo_sb.py
+++ ppo_sb.py
@@ -10,21 +10,26 @@ SAVE_NETWORK_PATH = 'saved_networks/PPO_stable_baselines/PPO2_' + ENV_NAME
 SAVE_SUMMARY_PATH = 'summary/PPO_stable_baselines/PPO2_' + ENV_NAME
 TRAIN = True
 
-env = make_atari_env(ENV_NAME, num_env=4, seed=0)
+num_env = 1
+if TRAIN:
+    num_env = 4
+env = make_atari_env(ENV_NAME, num_env=num_env, seed=0)
 env = VecFrameStack(env, n_stack=4)
 
 if TRAIN:
     model = PPO2(CnnPolicy, env, verbose=1, tensorboard_log=SAVE_SUMMARY_PATH)
-    model.learn(total_timesteps=120000000)
+    model.learn(total_timesteps=20000000)
     model.save(SAVE_NETWORK_PATH)
 
     del model
 else:
     model = PPO2.load(SAVE_NETWORK_PATH)
     obs = env.reset()
+    cnt = 0
     while True:
         action, _states = model.predict(obs)
         obs, rewards, done, info = env.step(action)
         env.render()
-        if done:
+        cnt += int(done)
+        if cnt==5:
             break
